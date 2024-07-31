74eeb2062795626fcb4ac613e0ba3c5b6b3b34f9
aaashikahamed <ahamedaashik071@gmail.com>
2024-07-31 15:23:56 +0530

    List Rendoring and Conditional Logic

--- main.js
+++ main.js
@@ -6,7 +6,17 @@ const app = Vue.createApp({
             inStock: true,
             inventory: 0,
             // onSale : true
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+              ],
+            sizes : [
+                {id:123, size: 'sm'},
+                {id:124, size: 'M'},
+                {id:125, size: 'L'},
+                {id:126, size: 'XL'},
+            ]
         }
     }
 })
