0f0475e03637e11e612a5a4ad04d2a4d73c3e5a5
aaashikahamed <ahamedaashik071@gmail.com>
2024-07-31 15:14:15 +0530

    Conditionaly Logic

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image : './assets/images/socks_green.jpg'
+            image : './assets/images/socks_green.jpg',
+            inStock: true,
+            inventory: 0,
+            // onSale : true
+            details: ['50% cotton', '30% wool', '20% polyester']
         }
     }
 })
